711efbbc7bd5b37b52c5faf05352b206d2e51b53
luisparf <luispauloaraujoafonso@gmail.com>
2023-04-05 01:54:24 -0300

    att

--- funcs.py
+++ funcs.py
@@ -8,6 +8,9 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+
 import matplotlib.pyplot as plt
 
 # Pré processamento #################################################################################
@@ -37,35 +40,72 @@ def load_data():
     data = pd.read_csv('train.csv')
     train_data = data['selected_text'].apply(lambda x: clean_text(x))
     text_data = data['text'].apply(lambda x: clean_text(x))
-    train_target = data['sentiment']
-    return train_data, text_data, train_target
+    target = data['sentiment']
+    return train_data, text_data, target
 
 ######################################################################################################
 
-def run(X, Y):
+def run(train_data, test_data, Y):
 
-    kfolds = 5
-    f1scores = []
+    kfolds = 10
+    f1scores_clr = []
+    f1scores_cmnb = []
+    f1scores_crf = []
 
     for i in range(kfolds):
-        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=i)
+        # X_train, _, y_train, _ = train_test_split(train_data, Y, test_size=0.2, random_state=i)
+        X_train, X_test, y_train, y_test = train_test_split(train_data, Y, test_size=0.2, random_state=i)
+        # _, X_test, _, y_test = train_test_split(test_data, Y, test_size=0.2, random_state=i)
         tfidf_vectorizer = TfidfVectorizer()
         X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
         X_test_tfidf = tfidf_vectorizer.transform(X_test)
-        clf = LogisticRegression()
-        clf.fit(X_train_tfidf, y_train)
-        y_pred = clf.predict(X_test_tfidf)
-        f1score = f1_score(y_test, y_pred, average='macro')
-        f1scores.append(f1score)
+        
+        ## Regressão logística
+        clr = LogisticRegression()
+        clr.fit(X_train_tfidf, y_train)
+        y_pred = clr.predict(X_test_tfidf)
+        f1score_clr = f1_score(y_test, y_pred, average='macro')
+        f1scores_clr.append(f1score_clr)
+        print(f1score_clr)
 
-    mean_f1score = sum(f1scores) / kfolds
+        ## Naive Bayes
+        cmnb = MultinomialNB()
+        cmnb.fit(X_train_tfidf, y_train)
+        y_pred = cmnb.predict(X_test_tfidf)
+        f1score_cmnb = f1_score(y_test, y_pred, average='macro')
+        f1scores_cmnb.append(f1score_cmnb)
+        print(f1score_cmnb)
+
+        ## Random Forest
+        crf = RandomForestClassifier(n_estimators=100)
+        crf.fit(X_train_tfidf, y_train)
+        y_pred = crf.predict(X_test_tfidf)
+        f1score_crf = f1_score(y_test, y_pred, average='macro')
+        f1scores_crf.append(f1score_crf)
+        print(f1score_crf)
+
+
+
+    # mean_f1score = sum(f1scores_clr) / kfolds
+    # offset = 10
+
 
     plt.grid(True)
-    plt.plot(range(kfolds), f1scores, marker='o', linestyle='dashed')
+    plt.plot(range(kfolds), f1scores_clr, marker='o', linestyle='dashed', label=f'Average LR Score {f1score_clr * 100:.2f}%', color='purple')
+    plt.plot(range(kfolds), f1scores_cmnb, marker='o', linestyle='dashed', label=f'Average NB Score {f1score_cmnb * 100:.2f}%', color='red')
+    plt.plot(range(kfolds), f1scores_crf, marker='o', linestyle='dashed', label=f'Average RF Score {f1score_crf * 100:.2f}%', color='green')
     plt.xlabel('Fold')
     plt.ylabel('F1 Score')
     plt.title(f'F1 Scores of {kfolds}-fold Cross Validation')
     plt.xticks(range(kfolds))
-    plt.axhline(y=mean_f1score, color='r', linestyle='--', label=f'Average F1 Score {mean_f1score* 100:.2f} %')
+    # plt.axhline(y=mean_f1score, color='r', linestyle='-', label=f'Average F1 Score {mean_f1score* 100:.2f} %')
     plt.legend()
+    # for i, score in enumerate(f1scores_clr):
+    #     plt.annotate(f'{score * 100:.2f}', xy=(i, score), xytext=(-10, 5), textcoords='offset points',ha='center', va='bottom')
+    # for i, j in zip(range(kfolds), f1scores_clr):
+    #     plt.annotate(f'{j * 100:.2f}%', xy=(i,j), xytext =(0.1 * offset, -offset * 0.5),  
+    #         textcoords ='offset points',ha='center', va='bottom')
+    plt.savefig('fig.png')
+
     plt.show()
+[]
--- main.py
+++ main.py
@@ -16,51 +16,9 @@ from funcs import  load_data, run
 
 if __name__ == '__main__':
     
-    train_data, text_data, train_target = load_data()
+    train_data, test_data, target = load_data()
     
-    # train_features = data.drop( 'textID', axis=1)
-    # train_features = data.drop(['sentiment', 'textID'], axis=1)
-
-
-    # print(train_features)
-    # train_features = train_features.drop('species', axis=1)]
-
-    # print(f"text  = {data['text'].shape}")
-    # print(f"selected text = {data['selected_text'].shape}")
-    # print(f"sentiment = {data['sentiment'].shape}")
-    # print(f"text/selected = {data[['textID', 'selected_text']].shape(axis=1)}")
-
-
-
-    # # Split the data into train and test sets
-    # X_train,_, y_train , _ = train_test_split(train_data, train_target, test_size=0.2, random_state=42)
-    # _ ,X_test, _,y_test = train_test_split(text_data, train_target, test_size=0.2, random_state=42)
-    # X_train,X_test, y_train ,y_test  = train_test_split(data['text'], train_target, test_size=0.2, random_state=42)
-
-    # print(f'X_train = {X_train}\n\n')
-    # print(f'y_train = {y_train}\n\n')
-    # print(f'X_test = {X_test}\n\n')
-    # print(f'y_test = {y_test}\n\n')
-
-    # for x in X_train:
-    #     print(x)
-
-    # # Vectorize the text data using TF-IDF
-    # tfidf_vectorizer = TfidfVectorizer()
-    # X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
-    # X_test_tfidf = tfidf_vectorizer.transform(X_test)
-
-    # # # Train a logistic regression model on the vectorized data
-    # clf = LogisticRegression()
-    # clf.fit(X_train_tfidf, y_train)
-
-    # # # Evaluate the model using F1 score
-    # y_pred = clf.predict(X_test_tfidf)
-    # f1score = f1_score(y_test, y_pred, average='macro')
-    # print('F1 score:', f1score)
-
-    # k_folds(text_data,train_target)
-    run(train_data,train_target)
+    run(train_data,test_data,target)
 
 
    
